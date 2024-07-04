0820f28b5418fe993bdcc1763c0909459df9e2e3
Rohit R M <163559461+Rohit-R-M@users.noreply.github.com>
2024-07-04 22:49:14 +0530

    Add files via upload

--- Bankers Deadlock Detection.c
+++ Bankers Deadlock Detection.c
@@ -1,104 +1,132 @@
 //Bankers Algorithm 
-#include<stdio.h> 
-int main() 
-{  
-    int n, m, i, j, k; 
-    int alloc[20][20], max[20][20], avail[20]; 
-    
-    printf("Enter the size n and m:\n"); 
-    scanf("%d%d", &n, &m); 
-    
-    printf("Enter the matrix of Maximum Allocation:\n"); 
-    for (i = 0; i < n; i++) 
-    { 
-        for (j = 0; j < m; j++) 
-        { 
-            scanf("%d", &max[i][j]); 
-        } 
-    } 
-    
-    printf("Enter the matrix of Allocation:\n"); 
-    for (i = 0; i < n; i++) 
-    { 
-        for (j = 0; j < m; j++) 
-        { 
-            scanf("%d", &alloc[i][j]); 
-        } 
-    } 
-    
-    printf("Enter the %d Available resources:\n",m); 
-    for (i = 0; i < m; i++) 
-    { 
-        scanf("%d", &avail[i]); 
-    } 
-    
-    int f[n], ans[n], ind = 0; 
-    
-    for (i = 0; i < n; i++) 
-    { 
-        ans[i] = 0; 
-    } 
-    
-    for (k = 0; k < n; k++) 
-    { 
-        f[k] = 0; 
-    } 
-    
-    int need[n][m]; 
-    for (i = 0; i < n; i++) 
-    { 
-        for (j = 0; j < m; j++) 
-        { 
-            need[i][j] = max[i][j] - alloc[i][j]; 
-        } 
-    } 
-    printf("Need matrix:\n");
-   	for(i=0;i<n;i++)
-   	{
-    	for(j=0;j<m;j++)
-   	    {
-   			printf("%d ",need[i][j]);
- 		}
-		printf("\n");
-	}
-    int y = 0; 
-    for (k = 0; k < 5; k++) 
-    { 
-        for (i = 0; i < n; i++) 
-        { 
-            if (f[i] == 0) 
-            { 
-                int flag = 0; 
-                for (j = 0; j < m; j++) 
-                { 
-                    if (need[i][j] > avail[j]) 
-                    { 
-                        flag = 1; 
-                        break; 
-                    } 
-                } 
-                if (flag == 0) 
-                { 
-                    ans[ind++] = i; 
-                    for (y = 0; y < m; y++) 
-                    { 
-                        avail[y] += alloc[i][j]; 
-                    } 
-                    f[i] = 1; 
-                } 
-            } 
-        } 
-    } 
-    printf("Following is the safe sequence:\n"); 
-    for (i = 0; i < n - 1; i++) 
-    { 
-        printf("P%d -> ", ans[i]); 
-    } 
-    printf("P%d\n", ans[n - 1]); 
-    
-    return 0; 
+
+#include <stdio.h>
+
+int allocation[10][10], max[10][10], available[10], need[10][10];
+int np, nr;
+
+void read_matrix(int matrix[10][10])
+{
+    int i, j;
+    for (i = 0; i < np; i++)
+    {
+        for (j = 0; j < nr; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
 }
+
+void display_matrix(int matrix[10][10])
+{
+    int i, j;
+    for (i = 0; i < np; i++)
+    {
+        printf("P%d ", i);
+        for (j = 0; j < nr; j++)
+        {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void calculate_need()
+{
+    int i, j;
+    for (i = 0; i < np; i++)
+    {
+        for (j = 0; j < nr; j++)
+        {
+            need[i][j] = max[i][j] - allocation[i][j];
+        }
+    }
+}
+
+
+
+void bankers()
+{
+    int i, j, k = 0, flag = 0;
+    int finish[10], safe[10];
+    for (i = 0; i < np; i++)
+        finish[i] = 0;
+
+    for (i = 0; i < np; i++)
+    {
+    	flag=0;
+        if (finish[i] == 0)
+        {
+            for (j = 0; j < nr; j++)
+            {
+                if (need[i][j] > available[j])
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                finish[i] = 1;
+                safe[k] = i;
+                k++;
+
+                for (j = 0; j < nr; j++)
+                    available[j] += allocation[i][j];
+                i = -1; 
+            }
+        }
+    }
+
+    flag = 0;
+    for (i = 0; i < np; i++)
+    {
+        if (finish[i] == 0)
+        {
+            printf("The system is in deadlock state\n");
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 0)
+    {
+        printf("\nThe system is in safe state\n");
+        printf("Safe sequence is: ");
+        for (i = 0; i < np; i++)
+        {
+            printf("P%d ", safe[i]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+	int j;
+	printf("Enter the number of processor:\n");
+	scanf("%d",&np);
+	printf("Enter the number of resources:\n");
+	scanf("%d",&nr);
+	printf("Enter the Max allocation:\n");
+	read_matrix(max);
+	printf("Enter the Allocation:\n");
+	read_matrix(allocation);
+	printf("Enter the %d Available:\n",nr);
+	for(j=0;j<nr;j++)
+		scanf("%d",&available[j]);
+	
+	printf("\nNeed:\n");
+	calculate_need();
+	display_matrix(need);
+	bankers();
+	printf("\n\n");
+	return 0;
+}
+
+
+
 /*
+
 Max Allocation
 7 5 3
 3 2 2
